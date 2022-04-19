16eaf817addb36ae160e0357045d858f7d045b06
MichaelCostanzaJr <77464599+MichaelCostanzaJr@users.noreply.github.com>
2022-04-18 19:49:09 -0700

    Class_2_Notes

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,43 +1,77 @@
-let students=[
-    {
-        //attr:value,
-        firstName:"Michael",
-        lastName:"Costanza",
-        age:25,
-        isActive:true,
-    },
-    {
-        firstName:"John",
-        lastName:"Doe",
-        age:25,
-        isActive:true,
-    },
-    {
-        firstName:"Jane",
-        lastName:"Doe",
-        age:25,
-        isActive:false,
-        hobbies:["Listen Music", "Read", "Play Zelda"],
-        address:{
-            street:"Palm street",
-            zip: "22769",
-            number: "262-k",
-    
-    }
-    }];
+    //let students=[
+       //     //attr:value,
+          //  firstName:"Michael",
+           // lastName:"Costanza",
+           // age:25,
+           // isActive:true,
+       // },
+        //{
+           // firstName:"John",
+           // lastName:"Doe",
+           // age:25,
+           // isActive:true,
+        //},
+        //{
+            //firstName:"Jane",
+//lastName:"Doe",
+           // age:25,
+            //isActive:false,
+            //hobbies:["Listen Music", "Read", "Play Zelda"],
+           // address:{
+                //street:"Palm street",
+                //zip: "22769",
+                //number: "262-k",
+        
+        //}
+        //}];
 
-console.log(students[0].firstName);
+//console.log(students[0].firstName);
 
-for(let i=0;i<students.length;i++){
-    tmp +=`<li>${studnets[i].firstName}</li>`;
-    console.log(tmp);
+//for(let i=0;i<students.length;i++){
+    //tmp +=`<li>${studnets[i].firstName}</li>`;
+   // console.log(tmp);
 
-    document.getElementById("students").innerHTML+=tmp;
+    ////document.getElementById("students").innerHTML+=tmp;
+//}
+
+//displayStudent();
+////let p = document.createElement("p");
+
+
+//let div=document.getElementById("students");
+//div.appendChild(p);
+
+//function Students(fname,lname,age,isActive){
+    //this.firstName=fname;
+    //this.lasName=lname;
+    //this.age=age;
+    //this.isActive=isActive;
+//}
+
+//let student1= new Students("Michael","Costanza",25,true);
+//let student2= new Students("Jack","Costanza",25,true);
+
+//console.log(student1,student2);
+
+function Sports(watersports, snowsports,courtsports){
+    this.waterSports=watersports;
+    this.snowSports=snowsports;
+    this.courtSports=courtsports;
 }
 
-displayStudent();
-let p = document.createElement("p");
+let example1= new Sports("Wakeboarding","Snowboarding","Tennis");
+let example2= new Sports("JetSki","Skiing","Basketball");
+
+console.log(example1,example2);
 
 
-let div=document.getElementById("students");
-div.appendChild(p);
+
+
+
+
+
+//console.log(students[0].firsName){ 
+
+//}
+
+//displayStudent();
--- scripts/register.js
+++ scripts/register.js
@@ -9,25 +9,48 @@ let petSalon ={
         open:"9:00 a.m.",
         close:"8:00 p.m."
     },
-    pets:[
-        {
+    pets:[]
+}
 
-        },
-        {
-
-        },
-        {
-
-        },
-    ]
+function Pet(name,age,gender,breed,service,ownername,contactphone){
+    this.dogName=name;
+    this.dogAge=age;
+    this.dogGender=gender;
+    this.dogBreed=breed;
+    this.typeService=service;
+    this.ownerName=ownername;
+    this.contactPhone=contactphone;
 
 }
 
-console.log(petSalon);
-function displaySalonInfo(){
-
+// get the info from the inputs and store the information
+let inputName=document.getElementById("dogName");
+let inputAge=document.getElementById("dogAge");
+let inputGender=document.getElementById("dogGender");
+let inputBreed=document.getElementById("dogBreed");
+let inputService=document.getElementById("dogService");
+let inputownerName=document.getElementById("ownerName");
+let inputcontactPhone=document.getElementById("contactPhone");
+function register(){
+    //create the pet
+    let thePet = new Pet(inputName.value,inputAge.value, inputGender.value,);
+    console.log(thePet);
+    //push the pet into the array
+    petSalon.push(thePet);
+    //clear the inputs
+    clearInputs();
 }
 
-function displayPetsNames(){
-    
+function clearInputs(){
+    inputName.value="";
 }
+
+
+
+let example1= new Pet("Jack",1,"male","german shepard","haircut","Michael","911");
+
+let example2= new Pet("Thor",12,"male","german shepard","haircut","Michael","911");
+
+let example3= new Pet("Sheba",14,"female","collie","nail trim","Michael","911");
+
+console.log(example1,example2,example3);
